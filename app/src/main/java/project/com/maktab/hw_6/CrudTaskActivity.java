859fef62f44751913f20be70132265be140bc4b6
Sepehr Sadri <sepehrsadri@gmail.com>
2018-12-22 23:33:38 +0330

    + crud task activity and fragment created and first UI set for fragment

---
+++ app/src/main/java/project/com/maktab/hw_6/CrudTaskActivity.java
@@ -0,0 +1,13 @@
+package project.com.maktab.hw_6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrudTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crud_task);
+    }
+}
