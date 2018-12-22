6209872ea2018677964bec2a424873def930ccde
Sepehr Sadri <sepehrsadri@gmail.com>
2018-12-22 22:37:01 +0330

    + basic root , view pager, float button , tab layout added.

--- app/src/main/java/project/com/maktab/hw_6/MainActivity.java
+++ app/src/main/java/project/com/maktab/hw_6/MainActivity.java
@@ -1,13 +1,19 @@
 package project.com.maktab.hw_6;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabLayout = findViewById(R.id.tab_layout);
+        mViewPager = findViewById(R.id.view_pager);
     }
 }
