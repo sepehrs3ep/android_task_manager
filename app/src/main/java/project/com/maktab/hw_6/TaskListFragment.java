7ff42cbb70213d8b7858d283dbceb48df41af1d7
Sepehr Sadri <sepehrsadri@gmail.com>
2018-12-23 22:00:25 +0330

    + task list adapter and view holder and getInstance added.

--- app/src/main/java/project/com/maktab/hw_6/TaskListFragment.java
+++ app/src/main/java/project/com/maktab/hw_6/TaskListFragment.java
@@ -2,23 +2,67 @@ package project.com.maktab.hw_6;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import project.com.maktab.hw_6.model.Task;
+import project.com.maktab.hw_6.model.TaskRepository;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TaskListFragment extends Fragment {
-private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
+    private static final String ARGS_LIST_STATUS = "project.com.maktab.hw_6.args_list_status";
+    private TaskAdapter mTaskAdapter;
+    private boolean mListStatus;
 
     public TaskListFragment() {
         // Required empty public constructor
     }
 
+    public static TaskListFragment getInstance(boolean listStatus) {
+        Bundle args = new Bundle();
+        args.putBoolean(ARGS_LIST_STATUS, listStatus);
+        TaskListFragment fragment = new TaskListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private void updateUI() {
+        List<Task> list;
+        if (mListStatus)
+            list = TaskRepository.getInstance().getAllList();
+        else list = TaskRepository.getInstance().getDoneList();
+
+        if (mTaskAdapter == null) {
+            mTaskAdapter = new TaskAdapter(list);
+            mRecyclerView.setAdapter(mTaskAdapter);
+        } else {
+            mTaskAdapter.setTaskList(list);
+            mTaskAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*true>> all list
+        false >> doneList*/
+        mListStatus = getArguments().getBoolean(ARGS_LIST_STATUS);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,7 +70,59 @@ private RecyclerView mRecyclerView;
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_task_list, container, false);
         mRecyclerView = view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        updateUI();
         return view;
     }
+
+    private class TaskViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTextViewTitle;
+        private TextView mTextViewImage;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextViewTitle = itemView.findViewById(R.id.item_title);
+            mTextViewImage = itemView.findViewById(R.id.item_circle_view);
+        }
+
+        public void bind(Task task) {
+            mTextViewTitle.setText(task.getTitle());
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+        private List<Task> mTaskList;
+
+        public void setTaskList(List<Task> taskList) {
+            mTaskList = taskList;
+        }
+
+        public TaskAdapter(List<Task> tasks) {
+            this.mTaskList = tasks;
+        }
+
+
+        @NonNull
+        @Override
+        public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.task_list_item, viewGroup, false);
+            TaskViewHolder taskViewHolder = new TaskViewHolder(view);
+            return taskViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskViewHolder taskViewHolder, int i) {
+            Task task = mTaskList.get(i);
+            taskViewHolder.bind(task);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTaskList.size();
+        }
+    }
+
 
 }
